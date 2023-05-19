654fbbc54e8398937f0d8f11da35f174ffffe3e9
FlyingDojo <67979735+FlyingDojo@users.noreply.github.com>
2023-05-19 21:54:02 +0100

    main.lua 12 attempt to call method draw a nil value

--- game.lua
+++ game.lua
@@ -12,7 +12,7 @@ function Game:new()
     game.menuOptions = {"Start", "Instructions", "Quit"}
     game.menuSelection = 1
     game.score = 0
-    game.player = nil -- Initialize player as nil
+    game.player = nil
     game.enemies = {}
     game.bullets = {}
     game.powerups = {}
@@ -84,6 +84,52 @@ function Game:update(dt)
         if self.player.lives <= 0 then
             self.state = Constants.GAME_STATES.GAME_OVER
         end
+    end
+end
+
+function Game:draw()
+    if self.state == Constants.GAME_STATES.PLAY then
+        -- Draw player
+        self.player:draw()
+
+        -- Draw bullets
+        for _, bullet in ipairs(self.bullets) do
+            bullet:draw()
+        end
+
+        -- Draw enemies
+        for _, enemy in ipairs(self.enemies) do
+            enemy:draw()
+        end
+
+        -- Draw power-ups
+        for _, powerup in ipairs(self.powerups) do
+            powerup:draw()
+        end
+
+        -- Draw score
+        love.graphics.print("Score: " .. self.score, 10, 10)
+        love.graphics.print("Lives: " .. self.player.lives, 10, 30)
+    elseif self.state == Constants.GAME_STATES.MENU then
+        -- Draw menu
+        love.graphics.setColor(1, 1, 1) -- Set color to white
+        love.graphics.setFont(Constants.FONT_MEDIUM)
+        local menuY = Constants.WINDOW_HEIGHT / 2 - (#self.menuOptions * Constants.FONT_MEDIUM:getHeight()) / 2
+        for i, option in ipairs(self.menuOptions) do
+            if i == self.menuSelection then
+                love.graphics.setColor(1, 0, 0) -- Highlight the selected option
+            else
+                love.graphics.setColor(1, 1, 1) -- Set color to white for other options
+            end
+            love.graphics.printf(option, 0, menuY + (i - 1) * Constants.FONT_MEDIUM:getHeight(), Constants.WINDOW_WIDTH, "center")
+        end
+    elseif self.state == Constants.GAME_STATES.GAME_OVER then
+        -- Draw game over message
+        love.graphics.setFont(Constants.FONT_LARGE)
+        love.graphics.setColor(1, 1, 1)
+        love.graphics.printf("Game Over", 0, Constants.WINDOW_HEIGHT / 2 - Constants.FONT_LARGE:getHeight() / 2, Constants.WINDOW_WIDTH, "center")
+        love.graphics.setFont(Constants.FONT_MEDIUM)
+        love.graphics.printf("Press Enter to restart", 0, Constants.WINDOW_HEIGHT / 2 + Constants.FONT_LARGE:getHeight() / 2, Constants.WINDOW_WIDTH, "center")
     end
 end
 
